78dee1745114a0e23d64de16fb4f530021a9f7e4
Sammie oje <omokhojesamuel8@gmail.com>
2025-09-16 21:44:09 +0100

    image path fix

--- src/js/data.js
+++ src/js/data.js
@@ -1,8 +1,8 @@
 export const data = {
   currentUser: {
     image: {
-      png: "/images/avatars/image-juliusomo.png",
-      webp: "/images/avatars/image-juliusomo.webp",
+      png: "./images/avatars/image-juliusomo.png",
+      webp: ".  /images/avatars/image-juliusomo.webp",
     },
     username: "juliusomo",
   },
@@ -15,8 +15,8 @@ export const data = {
       score: 12,
       user: {
         image: {
-          png: "/images/avatars/image-amyrobson.png",
-          webp: "/images/avatars/image-amyrobson.webp",
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
         },
         username: "amyrobson",
       },
@@ -30,8 +30,8 @@ export const data = {
       score: 5,
       user: {
         image: {
-          png: "/images/avatars/image-maxblagun.png",
-          webp: "/images/avatars/image-maxblagun.webp",
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
         },
         username: "maxblagun",
       },
@@ -45,8 +45,8 @@ export const data = {
           replyingTo: "maxblagun",
           user: {
             image: {
-              png: "/images/avatars/image-ramsesmiron.png",
-              webp: "/images/avatars/image-ramsesmiron.webp",
+              png: "./images/avatars/image-ramsesmiron.png",
+              webp: "./images/avatars/image-ramsesmiron.webp",
             },
             username: "ramsesmiron",
           },
@@ -60,8 +60,8 @@ export const data = {
           replyingTo: "ramsesmiron",
           user: {
             image: {
-              png: "/images/avatars/image-juliusomo.png",
-              webp: "/images/avatars/image-juliusomo.webp",
+              png: "./images/avatars/image-juliusomo.png",
+              webp: "./images/avatars/image-juliusomo.webp",
             },
             username: "juliusomo",
           },
